869c87caa1580116f9a2a8f860fab8cf8ecf132a
Alex Suspiro <alexandresuspiro@gmail.com>
2022-02-09 17:31:58 +0000

    render both results for query matches and recommended results

--- src/app/dashboard/query/query.component.ts
+++ src/app/dashboard/query/query.component.ts
@@ -3,6 +3,7 @@ import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { mock_results } from '../results-list/result.model';
 import { Result } from '../results-list/result.model';
+import { lastValueFrom } from 'rxjs';
 
 
 @Component({
@@ -31,36 +32,73 @@ export class QueryComponent implements OnInit {
       console.log(query)
 
       let results: Result[] = [];
+      // let movieId_for_recommendation: Number;
 
-      let url = `http://127.0.0.1:8080/matches/${event.target.value}`;
-      await this.http.get(url, { responseType: 'text' })
-        .subscribe(data => {
-          // console.log(data);
-          results = [];
-          let movies_json = JSON.parse(data)
-          console.log(movies_json)
-          for (let movie_json of movies_json) {
-            // console.log(movie_json['title'])
-            let result = new Result(movie_json['title'], movie_json['countries'], 'Test Description', movie_json['providers']);
-            results.push(result)
-            // console.log(movie_json)
-          }
-          console.log(results)
-          this.resultsReceivedEvent.emit(results)
-        }, err => console.log("Error:", err))
+      try {
+        //fetch movieIds matching query
+        let query_matching_url = `http://127.0.0.1:8080/matches/${event.target.value}`;
+        await lastValueFrom(this.http.get(query_matching_url, { responseType: 'text' }))
+          .then(movie_data => {
+            let movies = JSON.parse(movie_data)
+            results.push(...renderResults(movies))
+            // console.log(results)
+            this.resultsReceivedEvent.emit(results)
+            let movieId_for_recommendation: Number = movies[0].movieId;
+            return movieId_for_recommendation
 
-      // //mock case for when no results are found with given query
-      // if (query.length > 5) {
-      //   this.resultsReceivedEvent.emit([]);
-      //   return
-      // }
+          }).then(async movieId => {
+            console.log(movieId)
+            let recommendations_url = `http://127.0.0.1:8080/recommendations/${movieId}`;
+            await lastValueFrom(this.http.get(recommendations_url, { responseType: 'text' }))
+              .then(data => {
+                let recommended_movies = JSON.parse(data)
+                results.push(...renderResults(recommended_movies))
+                this.resultsReceivedEvent.emit(results)
+              })
+          })
+          .catch(err => console.log('err!: ', err))
 
-      // let result1 = this.mock_results[Math.floor(Math.random() * this.mock_results.length)]
-      // let result2 = this.mock_results[Math.floor(Math.random() * this.mock_results.length)]
-      // let results = [result1, result2]
+        console.log(results)
+
+        console.log('Resulst received event emitted!')
+      } catch (err) {
+        console.log("error!: ", err)
+      }
+
+
+      // let query_matching_url = `http://127.0.0.1:8080/matches/${event.target.value}`;
+      // // let url = `http://127.0.0.1:8080/recommendations/2640`;
+      // await this.http.get(query_matching_url, { responseType: 'text' })
+      //   .subscribe(data => {
+      //     let movie_objects = JSON.parse(data)
+      //     console.log(movie_objects)
+
+      //     let results: Result[] = []
+      //     results = renderResults(movie_objects);
+      //     console.log(results)
+      //     this.resultsReceivedEvent.emit(results)
+
+
+      //   }, err => console.log("Error:", err))
+
+      // let recommedations_url = `http://127.0.0.1:8080/recommendations/${movies_json[0].movieId}`;
+      // await this.http.get(recommedations_url, { responseType: 'text' })
+      //   .subscribe(data => {
+
+      //   })
+
 
     }
 
+    function renderResults(movies: []) {
+      let results: Result[] = [];
+      console.log(movies)
+      for (let movie of movies) {
+        let result = new Result(movie['title'], movie['countries'], 'Test Description', movie['providers']);
+        results.push(result)
+      }
+      return results
+    }
     //API call GET /results_from_query
 
     //push received results onto results-list 
