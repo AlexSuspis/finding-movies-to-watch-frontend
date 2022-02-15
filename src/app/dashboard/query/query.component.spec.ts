7b4dc0c9d9cb25d4bc442d5a84c6ef122be7f57d
Alex Suspiro <alexandresuspiro@gmail.com>
2022-02-15 13:53:32 +0000

    write some tests

--- src/app/dashboard/query/query.component.spec.ts
+++ src/app/dashboard/query/query.component.spec.ts
@@ -23,33 +23,20 @@ describe('QueryComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('First time using app, no previous searches', () => {
-    it('should perform an API call to GET /recommendations to get a set of recommended movies and populate results-list with them', () => { })
-  })
+  // describe('First time using app, no previous searches', () => {
+  //   it('should perform an API call to GET /recommendations to get a set of recommended movies and populate results-list with them', () => { })
+  // })
   describe('Each time user changes the text in input element', () => {
 
-    it('should trigger an input event', () => {
-
-      describe('If input element is empty', () => {
-        it('should hide the delete icon', () => { });
-        it('should display a placeholder in input element saying "Insert your query here"', () => { });
-        it('should display a search icon ', () => { });
-      });
-
-      describe('If input element is not empty', () => {
-        it('should hide the search icon', () => { });
-        it('should display a cross icon', () => { });
-      });
-    });
-    it('should trigger an API call to /GET results with input text as payload', () => { });
-
-    describe('When a non-empty set of results is received from /GET results', () => {
-      it('should delete all results currently stored in the results list in results-list component', () => { });
-      it('should push the received query results onto a results list in results-list component', () => { });
-    });
-    describe('When an empty set of results is received from /GET results', () => {
-      it('display "Query not found" message in results-list component', () => { })
-
-    })
   });
+
+  describe('When a non-empty set of results is received from /GET results', () => {
+    it('should delete all results currently stored in the results list in results-list component', () => { });
+    it('should push the received query results onto a results list in results-list component', () => { });
+    it('should then call GET /recommendations/:movieId', () => { })
+  });
+
+  describe('When an empty set of results is received from /GET matches/:query', () => {
+    it('display "Query not found" message in results-list component', () => { })
+  })
 });
