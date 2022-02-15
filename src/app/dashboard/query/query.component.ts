211f32e6d85d2630bc69a4e5eb55c1ce748c2129
Alex Suspiro <alexandresuspiro@gmail.com>
2022-02-15 15:11:31 +0000

    implement switchMap operator so that only the latest http request is listened to. This stops old http requests rendering in case they take longer in serverside than the newer requests

--- src/app/dashboard/query/query.component.ts
+++ src/app/dashboard/query/query.component.ts
@@ -3,8 +3,7 @@ import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { mock_results } from '../results-list/result.model';
 import { Result } from '../results-list/result.model';
-import { lastValueFrom } from 'rxjs';
-
+import { debounceTime, distinctUntilChanged, lastValueFrom, of, Subject, switchMap, map } from 'rxjs';
 
 @Component({
   selector: 'app-query',
@@ -18,7 +17,21 @@ export class QueryComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
+  private requestSource = new Subject<void>();
+
+  request() {
+    this.requestSource.next();
+  }
+
   ngOnInit(): void {
+    // this.requestSource.pipe(switchMap(req => this.http.get(query_matching_url, { responseType: 'text' })))
+    //   .subscribe(moviesJSON => {
+    //     let movies = JSON.parse(moviesJSON)
+    //     console.log(movies)
+    //     results.push(...renderResults(movies));
+    //     this.resultsReceivedEvent.emit(results)
+    //   });
+
   }
 
   async onInputChange(event: any) {
@@ -36,29 +49,70 @@ export class QueryComponent implements OnInit {
 
       try {
         //fetch movieIds matching query
+
         let query_matching_url = `http://127.0.0.1:8080/matches/${event.target.value}`;
-        await lastValueFrom(this.http.get(query_matching_url, { responseType: 'text' }))
-          .then(movie_data => {
-            let movies = JSON.parse(movie_data)
-            results.push(...renderResults(movies))
-            // console.log(results)
-            this.resultsReceivedEvent.emit(results)
-            let movieId_for_recommendation: Number = movies[0].movieId;
-            return movieId_for_recommendation
 
-          }).then(async movieId => {
-            console.log(movieId)
-            let recommendations_url = `http://127.0.0.1:8080/recommendations/${movieId}`;
-            await lastValueFrom(this.http.get(recommendations_url, { responseType: 'text' }))
-              .then(data => {
-                let recommended_movies = JSON.parse(data)
-                results.push(...renderResults(recommended_movies))
-                this.resultsReceivedEvent.emit(results)
+        // Heavily inspired by Suresh Kumar Aryia's answer in https://stackoverflow.com/questions/53432096/observables-cancel-previous-http-request-on-new-subscription-call
+        of(query).pipe(
+          debounceTime(400),
+          distinctUntilChanged(),
+          switchMap(() => {
+            return this.http.get(query_matching_url, { responseType: 'text' }).pipe(
+              map((moviesJSON: any) => {
+                return moviesJSON;
               })
+            )
           })
-          .catch(err => console.log('err!: ', err))
+        ).subscribe(moviesJSON => {
+          let movies = JSON.parse(moviesJSON)
+          console.log(movies)
+          results.push(...renderResults(movies));
+          this.resultsReceivedEvent.emit(results)
+        });
 
-        console.log(results)
+
+        // this.http.get(query_matching_url, { responseType: 'text' })
+        //   .subscribe(moviesJSON => {
+        //     let movies = JSON.parse(moviesJSON)
+        //     console.log(movies)
+        //     results.push(...renderResults(movies));
+        //     this.resultsReceivedEvent.emit(results)
+
+        //     let movieId_for_recommendation: Number = movies[0].movieId;
+        //     let recommendations_url = `http://127.0.0.1:8080/recommendations/${movieId_for_recommendation}`;
+        //     this.http.get(recommendations_url, { responseType: 'text' })
+        //       .subscribe(moviesJSON => {
+        //         let movies = JSON.parse(moviesJSON)
+        //         console.log(movies)
+        //         results.push(...renderResults(movies));
+        //         this.resultsReceivedEvent.emit(results)
+
+        //       });
+        //   });
+
+
+        // await lastValueFrom(this.http.get(query_matching_url, { responseType: 'text' }))
+        //   .then(movie_data => {
+        //     let movies = JSON.parse(movie_data)
+        //     results.push(...renderResults(movies))
+        //     // console.log(results)
+        //     this.resultsReceivedEvent.emit(results)
+        //     let movieId_for_recommendation: Number = movies[0].movieId;
+        //     return movieId_for_recommendation
+
+        //   }).then(async movieId => {
+        //     console.log(movieId)
+        //     let recommendations_url = `http://127.0.0.1:8080/recommendations/${movieId}`;
+        //     await lastValueFrom(this.http.get(recommendations_url, { responseType: 'text' }))
+        //       .then(data => {
+        //         let recommended_movies = JSON.parse(data)
+        //         results.push(...renderResults(recommended_movies))
+        //         this.resultsReceivedEvent.emit(results)
+        //       })
+        //   })
+        //   .catch(err => console.log('err!: ', err))
+
+        // console.log(results)
 
         console.log('Resulst received event emitted!')
       } catch (err) {
