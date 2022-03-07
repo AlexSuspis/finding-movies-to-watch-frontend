431018a4ed55bd9fe689d27611d65f74b2b6ecac
Alex Suspiro <alexandresuspiro@gmail.com>
2022-03-07 19:25:49 +0000

    finish filtering by country and providers

--- src/app/dashboard/results-list/view-preferences/view-preferences.component.ts
+++ src/app/dashboard/results-list/view-preferences/view-preferences.component.ts
@@ -12,23 +12,30 @@ export class ViewPreferencesComponent implements OnInit {
 
   @Output() filterResultsEvent = new EventEmitter();
   isFilterActive: boolean = false;
+  activeLiNumber: Number = -1;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onClick(propertyLabel: any, $event: any) {
+  onClick(propertyLabel: any, $event: any, liNumber: any) {
     console.log(propertyLabel)
+    console.log($event)
+    console.log(this.activeLiNumber)
     if (propertyLabel == 'reset') {
       this.isFilterActive = false;
       $event.property = propertyLabel;
+      //set the class of all elements inside .dropdown-menu to .dropdown-item
     }
     else {
       this.isFilterActive = true;
       $event.property = propertyLabel;
       console.log($event)
+      //set the class of element to .dropdown-item active
     }
+    this.activeLiNumber = liNumber
+    console.log(this.activeLiNumber)
     this.filterResultsEvent.emit($event);
   }
 
