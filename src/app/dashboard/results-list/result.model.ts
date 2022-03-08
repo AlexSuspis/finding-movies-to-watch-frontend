ff448c27c9e724f60d3f2641b32b200c0683d397
Alex Suspiro <alexandresuspiro@gmail.com>
2022-03-08 11:19:59 +0000

    render images of country flags instead of text. include title in image so user can see the country when hovering

--- src/app/dashboard/results-list/result.model.ts
+++ src/app/dashboard/results-list/result.model.ts
@@ -4,7 +4,8 @@ export class Result {
         // public url: String,
         public countries: String[],
         public description: String,
-        public streamingProviders: String[]
+        public streamingProviders: String[],
+        public country_flag_urls: String[]
     ) { }
 
     getDescription() {
@@ -13,12 +14,12 @@ export class Result {
 }
 
 export var mock_results = [
-    new Result('Saving Private Ryan', ['Portugal', 'United Kingdom'], 'Nice film', ['Amazon', 'Netflix', 'HBO']),
-    new Result('Fury', ['United Kingdom'], 'cool', ['HBO']),
-    new Result('Amelie', ['France'], 'Great', ['Netflix']),
-    new Result('The Pianist', ['United States'], 'cool', ['Netflix', 'Amazon Prime']),
-    new Result('Die Hard', ['India', 'Portugal'], 'Nice film', ['Amazon', 'Netflix', 'HBO']),
-    new Result('Shrek', ['United Kingdom', 'France'], 'cool', ['HBO']),
-    new Result('Suspiria', ['France'], 'Great', ['Netflix']),
-    new Result('Tree of Life', ['United States'], 'cool', ['Netflix', 'Amazon Prime']),
+    new Result('Saving Private Ryan', ['Portugal', 'United Kingdom'], 'Nice film', ['Amazon', 'Netflix', 'HBO'], ['http://www.geognos.com/api/en/countries/flag/PT.png', 'http://www.geognos.com/api/en/countries/flag/GB.png']),
+    new Result('Fury', ['United Kingdom'], 'cool', ['HBO'], ['http://www.geognos.com/api/en/countries/flag/GB.png']),
+    new Result('Amelie', ['France'], 'Great', ['Netflix'], ['http://www.geognos.com/api/en/countries/flag/FR.png']),
+    new Result('The Pianist', ['United States'], 'cool', ['Netflix', 'Amazon Prime'], ['http://www.geognos.com/api/en/countries/flag/US.png']),
+    new Result('Die Hard', ['India', 'Portugal'], 'Nice film', ['Amazon', 'Netflix', 'HBO'], ['http://www.geognos.com/api/en/countries/flag/IN.png', 'http://www.geognos.com/api/en/countries/flag/PT.png']),
+    // new Result('Shrek', ['United Kingdom', 'France'], 'cool', ['HBO']),
+    // new Result('Suspiria', ['France'], 'Great', ['Netflix']),
+    // new Result('Tree of Life', ['United States'], 'cool', ['Netflix', 'Amazon Prime']),
 ]
